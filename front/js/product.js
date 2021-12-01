ba711ceb538249011e261238b47edf5e754d98f2
Eric <eric.gravier@outlook.com>
2021-12-01 09:57:30 +0100

    JS recup datas serveur et affichage index.html

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,20 @@
 console.log('fichier product.js')
+
+// récuperation de l'id du canapé dans l'url
+
+let urlID = location.search.substring().split("=") // urlID[1] : id uniquement
+console.log(urlID[1])
+
+
+
+// requete sur le canapé selectionné
+
+let url = "http://localhost:3000/api/products/" + urlID[1] // requete API
+
+fetch(url)
+.then(response =>             // promesse réponse serveur
+    response.json()
+    .then((LeCanap) => {                  // promesse datas JSON
+        console.log(LeCanap);
+        
+    }))
--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,40 @@
-console.log('fichier script.js')
+//console.log('fichier script.js')
+
+
+let url = "http://localhost:3000/api/products"  // requete API
+
+
+// ecoute du clic sur canap sélectionne
+//document.querySelector("#").addEventListener('',function(){
+//})
+
+// requete vers serveur + promesse
+//const promiseCanaps = new Promise(function(resolve,reject)){}
+fetch(url)
+.then(response =>             // promesse réponse serveur
+    response.json()
+    .then((LesCanaps) => {                  // promesse datas JSON
+        //console.log(LesCanaps);           
+        
+        for (const UnCanap of LesCanaps) {  // pour chaque element de la liste
+            //console.log(UnCanap._id);
+            let ArticleHTML = `
+            <a href="./product.html?id=${UnCanap._id}">
+                <article>
+                    <img src="${UnCanap.imageUrl}" alt="${UnCanap.altTxt}">
+                    <h3 class="productName">${UnCanap.name}</h3>
+                    <p class="productDescription">${UnCanap.description}</p>
+                </article>
+            </a>`;
+            document.getElementById('items').innerHTML += ArticleHTML; // ajoute chaque article ds le doc HTML
+            
+        }
+    //.catch((erreur) => {      // erreur promesse JSON
+    //})
+
+    }))
+.catch((erreur) => {
+    // Erreur promesse du serveur
+    document.getElementById('items').innerHTML = `<p class="erreur">Serveur inaccessible, recommencer plus tard ...</p>`
+})
+
