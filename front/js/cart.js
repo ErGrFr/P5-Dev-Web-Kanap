7d2a4df2cdfcbdf29660bcfc8250306ade1c148f
Eric <eric.gravier@outlook.com>
2021-12-13 16:49:22 +0100

    JS Calcul et affichage Total panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,15 @@
 console.log('fichier cart.js');
 //let lesCanaps = JSON.parse(localStorage.getItem('selectionCanap'));
 
+// Total panier
+let panierTotal = {
+  prixTotal: 0,
+  qtyTotal:0,
+}
+
+
+
+
 
 
 // affichage du panier
@@ -10,36 +19,20 @@ for (const unCanap of lesCanaps) {
     //console.log(datasHTML);
 }
 
-// affichage un canap du panier
+console.log(panierTotal);
+// affichage du prix total du panier et qty total du panier ( le cumul se fait ds la boucle de lecture du localstorage )
+document.getElementById('totalQuantity').innerText = panierTotal.qtyTotal;
+document.getElementById('totalPrice').innerText = panierTotal.prixTotal;
+
+
+// affichage un canap du panier et renvoi le prix du canap
 
 function majUnCanap(leCanap){
-
-    /*let unCanap = `
-            <article class="cart__item" data-id="${leCanap.id}" data-color="${leCanap.color}">
-                <div class="cart__item__img">
-                  <img src="${leCanap.img}" alt="${leCanap.alt}">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${leCanap.name}</h2>
-                    <p>${leCanap.color}</p>
-                    <p>${leCanap.price} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : ${leCanap.qty}</p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-            </article>`;*/
     // selection du parent
     const leParent = document.getElementById("cart__items");
     //console.log(leParent);
-    // chargement template
+
+    // chargement template (un bloc temlate a été rajouté ds le html autour de "cart__item")
     const leTemplate = document.getElementById("template");
     //console.log(leTemplate);
 
@@ -48,34 +41,50 @@ function majUnCanap(leCanap){
     let leClone = document.importNode(leTemplate.content, true);
     //console.log(leClone);
 
-    // suppression de la balise template
-
     // modification des informations ds le clone
-    //clone.getElementsByTagName("img")[0].setAttribute("src","leCanap.img"); // modif de l'image
-    leClone.getElementById("ImgProduit").setAttribute("src",`${leCanap.img}`);
+    leClone.getElementById("ImgProduit").setAttribute("src",`${leCanap.img}`);  // modif de l'image
     leClone.getElementById("ImgProduit").setAttribute("alt",`${leCanap.alt}`);
-    //clone.getElementsByTagName("img").setAttribute("alt","leCanap.alt");
-    //console.log(clone.getElementsByTagName("h2"));
-    //console.log(`${leCanap.name}`);
     leClone.getElementById("NomProduit").textContent = `${leCanap.name}`;       // modif du nom
     leClone.getElementById("CouleurProduit").textContent = `Couleur : ${leCanap.color}`;    // modif de la couleur
     //console.log(leClone);
-    leClone.getElementById("PrixProduit").textContent = `Prix : ${leCanap.price} €`;    // modif du prix
+    
     leClone.getElementById("QtyProduit").textContent = `qté : ${leCanap.qty}`;    // modif de la qté
+    panierTotal.qtyTotal += parseInt(`${leCanap.qty}`);   //  recalcul de la qté panier
+    leClone.getElementById("PrixProduit").textContent = `Prix : ${leCanap.price} €`;    // modif du prix
+    panierTotal.prixTotal += parseInt(`${leCanap.price}`) * parseInt(`${leCanap.qty}`);   //  recalcul du prix panier
     //clone.querySelector(".cart__item__content__settings__quantity p").innerHTML = `Qté : ${leCanap.qty}`;  // modif qty
     leClone.getElementById("QtyProduitInput").setAttribute("value",`${leCanap.qty}`); // modif qty ds le input
+    
     // ajout du clone ds html
     //document.getElementById("cart_items").appendChild(leClone);
-    console.log(leClone);
+    //console.log(leClone);
     leParent.appendChild(leClone);
     //console.log(leClone);
 
-    //return unCanap;
-
 }
 
+// ecoute des boutons supprimer
+let boutonsSupprimmer = document.querySelectorAll('.deleteItem');
+console.log(boutonsSupprimmer);
 
-// requete de commande
+//for(unBouton of boutonsSupprimmer){
+  console.log(boutonsSupprimmer.length);
+for(i=0;i<boutonsSupprimmer.length;i++){
+  boutonsSupprimmer[i].addEventListener("click" , function(){
+    console.log(boutonsSupprimmer[i]);
+    console.log("clic" + i);
+    // suppression du canap correspondant, meme index que les boutons
+    //deleteCanap(boutonsSupprimmer.Nodelist.item);
+  });
+};
+
+// suppression d'un canap dans le local storage
+function deleteCanap(index){
+  console.log("suppression canap" + index)
+};
+
+
+// requete de la commande
 let url = "http://localhost:3000/api/order/" ; // requete API
 fetch(url)
 .then(response =>                   // promesse réponse serveur
